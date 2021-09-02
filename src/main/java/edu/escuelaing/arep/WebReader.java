42149b9e7e2d8867b7ff8ad5de21f449081dc003
DavidRiveraRvD <54150584+DavidRiveraRvD@users.noreply.github.com>
2021-09-02 11:00:53 -0500

    Avance

---
+++ src/main/java/edu/escuelaing/arep/WebReader.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.arep;
+
+import java.io.*;
+import java.net.*;
+
+public class WebReader {
+
+	public static void main(String[] args) throws Exception {
+		URL google = new URL("http://www.google.com/");
+        }
+
+}
+
